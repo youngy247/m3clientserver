4a8090d3368c2ad03385c577fff4c2e988db2bb7
Adam Young <adamyoungy678@gmail.com>
2023-03-28 12:06:33 +0100

    converted if else statment to ternary operator

--- exercise1.php
+++ exercise1.php
@@ -1,16 +1,11 @@
 <?php
 
-// Start the session
 session_start();
 
-// Check if the session variable for the visit count is set
-if (isset($_SESSION['visit_count'])) {
-    // Increment the visit count
-    $_SESSION['visit_count']++;
-} else {
-    // Set the initial visit count to 1
-    $_SESSION['visit_count'] = 1;
-}
+isset($_SESSION['visit_count'])
+    ? $_SESSION['visit_count']++
+    : $_SESSION['visit_count'] = 1;
+
 
 // Output the visit count
 echo "You have visited this page " . $_SESSION['visit_count'] . " times.";
