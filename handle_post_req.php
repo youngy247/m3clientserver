740edb7fdad659661b24914051370ae7f7b8056f
Adam Young <adamyoungy678@gmail.com>
2023-03-28 15:13:54 +0100

    if string is an empty string output = 'Name is empty'

--- handle_post_req.php
+++ handle_post_req.php
@@ -2,7 +2,7 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // collect value of input field
-    $name = 'Hello ' . $_POST['fname'];
+    $name = 'Hello ' . $_POST['fname'] || $_POST['fname'] === '';
     if (empty($name)) {
         echo "Name is empty";
     } else {
