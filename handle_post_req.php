05da1025452e5e9e34cc619ee3877ef055076755
Adam Young <adamyoungy678@gmail.com>
2023-03-28 15:35:03 +0100

    another example of using POST request

--- handle_post_req.php
+++ handle_post_req.php
@@ -1,12 +1,27 @@
 <?php
 
+//if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//    // collect value of input field
+//    $name = 'Hello ' . $_POST['fname'] || $_POST['fname'] === '';
+//    if (empty($name)) {
+//        echo "Name is empty";
+//    } else {
+//        echo $name;
+//    }
+//}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // collect value of input field
-    $name = 'Hello ' . $_POST['fname'] || $_POST['fname'] === '';
-    if (empty($name)) {
-        echo "Name is empty";
-    } else {
-        echo $name;
+    $greeting = 'Hello ';
+    $age = $_POST['age'];
+    if
+        (empty($age)) {
+        echo 'Please type a valid input for age';
+        exit();
+    }
+    elseif($age < 18) {
+        echo "You are under 18";
+    } {
+        echo $greeting . 'you are ' . $age . ' so you are 18 or over';
     }
 }
-
--- testpost.php
+++ testpost.php
@@ -1,6 +1,10 @@
 <?php
 
+//echo '<form action="handle_post_req.php" method="post" >'
+//  . 'Name: <input type="text" name="fname">'
+//  . '</form>';
+
 echo '<form action="handle_post_req.php" method="post" >'
-  . 'Name: <input type="text" name="fname">'
-  . '<button type="submit">Submit</button>'
-  . '</form>';
+    . 'Age: <input type="number" name="age">'
+    . '<button type="submit">Submit</button>'
+    . '</form>';
