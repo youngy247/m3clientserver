9161fbd851e3628391b3a75d637d2e99daa3d5ab
Adam Young <adamyoungy678@gmail.com>
2023-03-28 14:24:11 +0100

    created how many timed visited functionality with a cookie

--- exercise1.php
+++ exercise1.php
@@ -2,11 +2,20 @@
 
 session_start();
 
-isset($_SESSION['visit_count'])
-    ? $_SESSION['visit_count']++
-    : $_SESSION['visit_count'] = 1;
-
-
+//isset($_SESSION['visit_count'])
+//    ? $_SESSION['visit_count']++
+//    : $_SESSION['visit_count'] = 1;
+//
+//
 // Output the visit count
-echo "You have visited this page " . $_SESSION['visit_count'] . " times.";
+//echo "You have visited this page " . $_SESSION['visit_count'] . " times.";
 
+if (isset($_COOKIE['count'])) {
+    $visits = $_COOKIE['count'] + 1;
+} else {
+    $visits = 1;
+}
+
+setcookie('count', $visits);
+
+echo '<p>You have visited ' . $visits . ' times';
