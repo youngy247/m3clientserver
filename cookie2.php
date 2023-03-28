d4f70b031ab3b32a00075a10ccf5f74fe4f500d8
Adam Young <adamyoungy678@gmail.com>
2023-03-28 10:59:49 +0100

    starting a sessions and added a value to username

--- cookie2.php
+++ cookie2.php
@@ -1,3 +1,4 @@
 <?php
 
 setcookie('current-age', '', time() - 60 * 60); // an hour ago
+
---
+++ sesspage1.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+//  session_start() first checks if a session is already started and if
+//  not then it starts one
+
+//  We need session_start() before we can access $_SESSION (Super global variable)
+
+//  It must be the very first thing in your script. Even before any HTML tags
+
+$_SESSION['username'] = 'fred1234'; // create a new session variable
+
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
