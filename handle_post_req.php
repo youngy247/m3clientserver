88d675bfcb23aaec5798cc8fd5a4ae8368d4c26f
Adam Young <adamyoungy678@gmail.com>
2023-03-28 15:03:21 +0100

    added 'Hello ' before their name to appear upon clicking button

--- handle_post_req.php
+++ handle_post_req.php
@@ -2,7 +2,7 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // collect value of input field
-    $name = $_POST['fname'];
+    $name = 'Hello ' . $_POST['fname'];
     if (empty($name)) {
         echo "Name is empty";
     } else {
